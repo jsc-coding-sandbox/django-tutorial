506bab444f165159cc7b2e5dd3cb1880561cea21
John C <24364368+lvl-6@users.noreply.github.com>
2021-07-28 22:26:12 +0100

    Fleshed out the views - now you can vote, and view results!

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.db.models import F
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
     # Get 5 most recent questions (as determined by pub_date)
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
             'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're viewing question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    # No idea why the tutorial does it this way for this one, but I'm copying.
-    response = "You're viewing the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Back to the question voting form (detail view)
+        return render(
+                request, 'polls/detail.html',
+                {
+                    'question': question,
+                    'error_message': "You didn't select a choice.",
+                })
+    else:
+        # Use F instead of incrementing the instance to avoid race condition
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Redirect will prevent data being sent twice if user hits browser back button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
