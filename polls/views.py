742587920aa260bbe36f7bcfc0da5731f459ec93
John C <24364368+lvl-6@users.noreply.github.com>
2021-07-29 22:13:56 +0100

    Added a bunch of tests for the Index and Detail views, and fixed some bugs

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -12,13 +13,25 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the most recent five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the most recent five published questions.
+        """
+        # lte = Less Than or Equal
+        return Question.objects.filter(
+                pub_date__lte=timezone.now()
+                ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet (pub_date is in the
+        future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
