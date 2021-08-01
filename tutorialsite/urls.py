d4dc3a48168f81d673dcc2dab3496f59fb159560
John C <24364368+lvl-6@users.noreply.github.com>
2021-08-01 22:41:20 +0100

    Added a new app main, containing a main (home) page with some text.

--- tutorialsite/urls.py
+++ tutorialsite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('main.urls'), name="main"),
+    path('polls/', include('polls.urls'), name="polls"),
+    path('admin/', admin.site.urls, name="admin"),
 ]
